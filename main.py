14707db5e2a30d3f6b8dc793416cc977bb8bd545
Kaj Grant <41174383+kajgrant@users.noreply.github.com>
2023-01-27 00:20:39 -0800

    feat: implement initial functionality of post retrieval (#2)

--- main.py
+++ main.py
@@ -1,28 +1,94 @@
 import requests
 import json
+import time
 
-def main():
+API = 'https://oauth.reddit.com'
 
-    with open('credentials.json', 'r') as json_file:
-        credentials = json.load(json_file)
 
-    app_id = credentials['app_id']
-    secret = credentials['secret']
-    reddit_username = credentials['reddit_username']
-    reddit_password = credentials['reddit_password']
+def authenticate(headers, credentials):
 
-    auth = requests.auth.HTTPBasicAuth(app_id, secret)
+    auth = requests.auth.HTTPBasicAuth(
+        credentials['app_id'], credentials['secret'])
 
     data = {
-    'grant_type': 'password',
-    'username': reddit_username,
-    'password': reddit_password
+        'grant_type': 'password',
+        'duration': 'permanent',
+        'username': credentials['reddit_username'],
+        'password': credentials['reddit_password']
     }
 
+    post_res = requests.post('https://www.reddit.com/api/v1/access_token',
+                             auth=auth, data=data, headers=headers)
+
+    check_response_status(post_res)
+
+    token = post_res.json()['access_token']
+    headers['Authorization'] = 'bearer {}'.format(token)
+
+    get_res = requests.get(
+        'https://oauth.reddit.com/api/v1/me', headers=headers)
+    check_response_status(get_res)
+
+    return post_res
+
+
+def get_credentials():
+    credentials = {}
+
+    try:
+        with open('credentials.json', 'r') as json_file:
+            credentials = json.load(json_file)
+    except:
+        print('credentials.json not found, please enter credentials')
+        credentials['app_id'] = input('App id: ')
+        credentials['secret'] = input('secret')
+        credentials['reddit_username'] = input('reddit_username')
+        credentials['reddit_password'] = input('reddit_password')
+
+    return credentials
+
+
+def check_response_status(res):
+    if res.status_code != 200:
+        raise Exception('Error: Response Returned ' + str(res.status_code))
+    else:
+        print('Request returned ' + str(res.status_code))
+
+
+def get_current_post(headers, token):
+    res = requests.get('{}/r/bapcsalescanada/new'.format(API),
+                       headers=headers, params={'limit': '1'})
+    res.json()
+    return res.json()['data']['children'][0]
+
+
+def print_initial_posts(headers, token):
+    res = requests.get('{}/r/bapcsalescanada/new'.format(API),
+                       headers=headers, params={'limit': '10'})
+    for post in res.json()['data']['children']:
+        print(post['data']['title'] + '\n')
+
+
+def main():
     headers = {'User-Agent': 'salestrackerbot/0.0.1'}
-    res = requests.post('https://www.reddit.com/api/v1/access_token',
-    auth=auth, data=data, headers=headers)
-    print(res)
+
+    credentials = get_credentials()
+    res = authenticate(headers, credentials)
+    token = res.json()['access_token']
+
+    headers['Authorization'] = 'bearer {}'.format(token)
+
+    print_initial_posts(headers, token)
+    latest_post = get_current_post(headers, token)
+
+    while (1):
+
+        current_post = get_current_post(headers, token)
+        if latest_post['data']['name'] != current_post['data']['name']:
+            print(current_post['data']['name'])
+            latest_post = current_post
+        time.sleep(30)
+
 
 if __name__ == "__main__":
     main()
