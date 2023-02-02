769153944fd33c0f54e7a171a2301512f95b15a5
Kaj <kajgrant@gmail.com>
2023-02-02 10:44:38 -0800

    feat: add argument validation

--- salestracker.py
+++ salestracker.py
@@ -10,6 +10,20 @@ SORT_METHOD = 'new'
 SOUND_FLAG = True
 CUR_PATH = os.path.dirname(__file__)
 
+FILTER_OPTIONS = ['new', 'top', 'hot', 'rising', 'controversial']
+
+def validate_flags(args):
+    if args.num_initial != None:
+        if type(args.num_initial) != int:
+            raise Exception('Error: num_initial is not a integer')
+    if args.filter != None:
+        if args.filter not in FILTER_OPTIONS:
+            raise Exception('Error: filter not valid option, must be on of: ' + str(FILTER_OPTIONS))
+    if args.sound != None:
+        args.sound = args.sound.lower() == ('true' or 't')
+        if type(args.sound) != bool:
+            raise Exception('Error: sound must be either True or False')
+    return args
 
 def main(args):
 
@@ -17,8 +31,10 @@ def main(args):
 
     num_posts = args.num_initial or NUM_INTIAL_POSTS
     sorting = args.filter or SORT_METHOD
-    sound_flag = args.sound or SOUND_FLAG
-
+    sound_flag = args.sound
+    if args.sound == None:
+        sound_flag = SOUND_FLAG
+    
     print('Currently tracking the following subreddits:')
     print(get_subreddits())
     print('Sorting method: ' + sorting)
@@ -54,14 +70,14 @@ def main(args):
                     playsound(CUR_PATH + '\\resources\\notification.mp3')
         r_api.wait(10)  # wait for 10 seconds before refreshing again
 
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--num_initial", required=False)
     parser.add_argument("-f", "--filter", required=False)
     parser.add_argument("-s", "--sound", required=False)
-    args = parser.parse_args()
+    raw_args = parser.parse_args()
+    args = validate_flags(raw_args)
 
     try:
         main(args)
